f8a15b9d3a74fad4513bb8af6ef1b0e74300d906
Leeios <kellos@hotmail.fr>
2014-12-17 21:20:34 +0100

    key rule

--- js/player.js
+++ js/player.js
@@ -15,12 +15,7 @@ sand.define('Player', [
 		tpl: function() {
 			return {
 				tag: '.wrap-player.player' + this.id,
-				children: ['.player'],
-				events: {
-					click: function(e) {
-						this.fire('O', this.id);
-					}.bind(this)
-				}
+				children: ['.player']
 			}
 		},
 
@@ -31,11 +26,23 @@ sand.define('Player', [
 			this.el.innerHTML = this.pts;
 			this.el.style.width = (100 / this.nbPlayers) + '%';
 			this.el.style.backgroundColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+			document.addEventListener('keydown', this.fireEvent.bind(this));
 		},
 
 		setPts: function(diffPts) {
 			this.pts += diffPts;
 			this.el.innerHTML = this.pts;
+		},
+
+		fireEvent: function(e) {
+			console.log(e.keyCode, this.id)
+			if (this.id === 0 && e.keyCode === 32
+				|| this.id === 1 && e.keyCode === 37
+				|| this.id === 2 && e.keyCode === 38
+				|| this.id === 3 && e.keyCode === 39
+				|| this.id === 4 && e.keyCode === 40) {
+				this.fire('O', this.id);
+			}
 		}
 
 	})
